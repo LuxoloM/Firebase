07bdb53f0b38102f1b851b0dee35faeaf67e904e
Luxolo Menze <60965384+LuxoloM@users.noreply.github.com>
2022-10-24 17:59:35 +0200

    Update RealTime Database

---
+++ app/src/main/java/com/example/firebase/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.firebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DatabaseReference mDatabase;
+    Button btn;
+    EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference("GoldPrice").child("price");
+
+        btn =  findViewById(R.id.button);
+        editText =  findViewById(R.id.editTextNumber);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mDatabase.setValue(editText.getText().toString());
+            }
+        });
+    }
+}
